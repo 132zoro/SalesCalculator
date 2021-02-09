57f85975bb4bba9691162fb3f407bddf7e10867e
Sambhrant Dhakal <sdhakal1@unm.edu>
2021-02-08 23:45:05 -0700

    code modified Fixes#2

--- scripts/salescalc.js
+++ scripts/salescalc.js
@@ -5,10 +5,13 @@ function main() {
 }
 
 function calculateSales() {
+    // input
     Price=parseFloat(txPrice.value)
     TaxRate=parseFloat(txTaxRate.value)/100
+    //Processing
     Tax=Price*TaxRate
     Total=Price+Tax
+    //Output
     spTotal.innerHTML=Total.toFixed(2)
 }
 
